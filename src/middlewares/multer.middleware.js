aa687d2424653e402d0d9fc47631a82603654f49
anshugpt <anshugupta082002@gmail.com>
2025-05-02 16:47:33 +0530

    refactor: separate image and video storage configurations in multer middleware

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
+const imageStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
@@ -9,4 +9,44 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage: storage });
+const videoStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now());
+  },
+});
+
+const imageFileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|gif|webp/;
+  const ext = file.originalname
+    .toLowerCase()
+    .match(/\.(jpeg|jpg|png|gif|webp)$/);
+  const mime = allowedTypes.test(file.mimetype);
+
+  if (ext && mime) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed!"), false);
+  }
+};
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /mp4|mov|avi|mkv/;
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (allowedTypes.test(ext)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only video files are allowed!"), false);
+  }
+};
+
+export const uploadImage = multer({
+  storage: imageStorage,
+  fileFilter: imageFileFilter,
+});
+export const uploadVideo = multer({
+  storage: videoStorage,
+  fileFilter: fileFilter,
+});
