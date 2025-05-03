2ca2208228e2bf97af83f0daeb3f828f069271b9
anshugpt <anshugupta082002@gmail.com>
2025-05-03 21:40:09 +0530

    feat: reorganize imports and define routes for video retrieval, deletion, and thumbnail update

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
-import { uploadVideoAndImage } from "../middlewares/multer.middleware";
-import { uploadVideo } from "../controllers/video.controllers";
+import {
+  uploadImage,
+  uploadVideoAndImage,
+} from "../middlewares/multer.middleware";
+import {
+  uploadVideo,
+  getVideo,
+  deleteVideo,
+  updateThumbnail,
+} from "../controllers/video.controllers";
 
 const router = Router();
 
@@ -13,3 +21,9 @@ router.route("/upload").post(
   ]),
   uploadVideo
 );
+
+router.route("/:videoId").get(verifyJWT, getVideo);
+router.route("/:videoDocumentId/delete").delete(verifyJWT, deleteVideo);
+router
+  .route("/:videoDocumentId/update-thumbnail")
+  .patch(verifyJWT, uploadImage.single("image"), updateThumbnail);
