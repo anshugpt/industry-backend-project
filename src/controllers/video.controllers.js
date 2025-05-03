0a3011be2e096bd58a4e7161716ef3382ee1594a
anshugpt <anshugupta082002@gmail.com>
2025-05-03 12:05:12 +0530

    feat: add getVideo function to retrieve video details with aggregation

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -48,4 +48,152 @@ const uploadVideo = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, newVideo, "Video uploaded successfully"));
 });
 
-export { uploadVideo };
+const getVideo = asyncHandler(async (req, res) => {
+  // get video _id by params
+  // write aggregation pipeline
+  const { videoId } = req.params;
+  if (!videoId) {
+    throw new ApiError(400, "Video id is required");
+  }
+  const videoDetails = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "owner",
+        foreignField: "channel",
+        as: "channelSubscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "channelDetails",
+        pipeline: [
+          {
+            $project: {
+              userName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "_id",
+        foreignField: "video",
+        as: "comments",
+        pipeline: [
+          {
+            $sort: { createdAt: -1 },
+          },
+          {
+            $lookup: {
+              from: "likes",
+              localField: "_id", // refers to the comment id
+              foreignField: "comment",
+              as: "likesOnComment",
+            },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "ownerDetails",
+              pipeline: [{ $project: { userName: 1, avatar: 1 } }],
+            },
+          },
+          {
+            $addFields: {
+              ownerDetails: { $first: "$ownerDetails" },
+            },
+          },
+
+          {
+            $addFields: {
+              totalLikesOnComment: { $size: "$likesOnComment" },
+            },
+          },
+          {
+            $project: {
+              content: 1,
+              owner: 1,
+              createdAt: 1,
+              totalLikesOnComment: 1,
+              ownerDetails: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "video",
+        as: "likes",
+        pipeline: [
+          {
+            $project: {
+              likedBy: 1,
+              createdAt: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        channelSubscribers: { $size: "$channelSubscribers" },
+        channelDetails: {
+          $first: "$channelDetails",
+        },
+        totalComments: {
+          $size: "$comments",
+        },
+        comments: "$comments",
+        likes: {
+          $size: "$likes",
+        },
+        likedBy: "$likes.likedBy",
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        duration: 1,
+        views: 1,
+        isPublished: 1,
+        createdAt: 1,
+        channelSubscribers: 1,
+        channelDetails: 1,
+        totalComments: 1,
+        comments: 1,
+        likes: 1,
+        likedBy: 1,
+      },
+    },
+  ]);
+
+  if (!videoDetails || videoDetails.length === 0) {
+    throw new ApiError(404, "Video not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, videoDetails[0], "Video details fetched"));
+});
+
+export { uploadVideo, getVideo };
