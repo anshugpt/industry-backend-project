4db42ddc047b2e7695e28ff607cc544560508ff8
anshugpt <anshugupta082002@gmail.com>
2025-05-16 21:36:45 +0530

    feat: add playlist controller with create and fetch user playlists functionality

---
+++ src/controllers/playlist.controllers.js
@@ -0,0 +1,56 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Playlist } from "../models/playlist.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const createPlaylist = asyncHandler(async (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    throw new ApiError(400, "Name or description is required");
+  }
+
+  const playlist = await Playlist.create({
+    name: name.trim(),
+    description: description.trim(),
+    owner: req.user?._id,
+  });
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, playlist, "Playlist created successfully"));
+});
+
+const getUserPlaylists = asyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Required a valid user id");
+  }
+
+  const playlists = await Playlist.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        videos: 1,
+        createdAt: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, playlists, "Playlists fetched successfully"));
+});
+
+export { createPlaylist, getUserPlaylists };
