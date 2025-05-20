8f137c596663699915746f8108c443f700feac5c
anshugpt <anshugupta082002@gmail.com>
2025-05-20 20:12:46 +0530

    feat: implement playlist routes for managing playlists and videos

---
+++ src/routes/playlist.routes.js
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+  addVideoToPlaylist,
+  createPlaylist,
+  deletePlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+  removeVideoFromPlaylist,
+  updatePlaylist,
+} from "../controllers/playlist.controllers.js";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/").post(createPlaylist);
+router
+  .route("/:playlistId")
+  .get(getPlaylistById)
+  .patch(updatePlaylist)
+  .delete(deletePlaylist);
+router.route("/add/:playlistId/:videoId").patch(addVideoToPlaylist);
+router.route("/remove/:playlistId/:videoId").patch(removeVideoFromPlaylist);
+router.route("/user/:userId").get(getUserPlaylists);
+
+export default router;
