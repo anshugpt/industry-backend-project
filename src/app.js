1c106f58009c4de5db8dee4e15ecca2790a93fb2
anshugpt <anshugupta082002@gmail.com>
2025-05-23 14:49:28 +0530

    feat: reorganize router imports and declarations in app.js; remove obsolete healthcheck.routes.js

--- src/app.js
+++ src/app.js
@@ -17,8 +17,24 @@ app.use(cookieParser());
 
 // import routers
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscriptions.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import commentRouter from "./routes/comments.routes.js";
 
-// routers
+// routers declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export { app };
